b439090160e0db9fd97c8b798ae573815efcf326
stoffee <chris@roadkill.org>
2019-10-10 13:44:04 -0700

    added build_vm module

--- modules/build_vm/main.tf
+++ modules/build_vm/main.tf
@@ -6,24 +6,24 @@ provider "nutanix" {
   insecure = true
   port     = 9440
 }
-data "terraform_remote_state" "image" {
-  backend = "remote"
-
-  config = {
-    organization = "cdunlap"
-    workspaces = {
-      name = "nutanix-terraform"
-    }
-  }
-}
-
-resource "nutanix_image" "linux" {
-    image_id = data.nutanix_image.linux.id
-}
-
-#data "nutanix_image" "ubuntu" {
-#  image_id = "2b4b5942-2c0e-45eb-b17e-a10b8479c824"
+#data "terraform_remote_state" "image" {
+#  backend = "remote"
+#
+#  config = {
+#    organization = "cdunlap"
+#    workspaces = {
+#      name = "nutanix-terraform"
+#    }
+#  }
 #}
+
+#resource "nutanix_image" "linux" {
+#    image_id = data.nutanix_image.linux.id
+#}
+
+data "nutanix_image" "ubuntu" {
+  image_id = data.module.nutanix_image.linux.id
+}
 
 data "nutanix_clusters" "clusters" {}
 
@@ -36,14 +36,4 @@ resource "nutanix_virtual_machine" "test" {
   memory_size_mib      = 4096
 }
 
-variable "username" {
-  description = "admin username for Nutanix Cluster"
-}
 
-variable "password" {
-  description = "admin password for Nutanix Cluster"
-}
-
-variable "endpoint" {
-  description = "ip or hostname for Nutanix Cluster"
-}
---
+++ modules/build_vm/variables.tf
@@ -0,0 +1,11 @@
+variable "username" {
+  description = "admin username for Nutanix Cluster"
+}
+
+variable "password" {
+  description = "admin password for Nutanix Cluster"
+}
+
+variable "endpoint" {
+  description = "ip or hostname for Nutanix Cluster"
+}
