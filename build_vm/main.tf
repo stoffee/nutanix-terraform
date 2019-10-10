e464a521ecb090a6bf77d71da0d584d65c25d829
stoffee <chris@roadkill.org>
2019-10-10 11:48:18 -0700

    added image and vm creation

---
+++ build_vm/main.tf
@@ -0,0 +1,35 @@
+
+provider "nutanix" {
+  username = var.username
+  password = var.password
+  endpoint = var.endpoint
+  insecure = true
+  port     = 9440
+}
+
+data "nutanix_image" "ubuntu" {
+  image_id = "2b4b5942-2c0e-45eb-b17e-a10b8479c824"
+}
+
+data "nutanix_clusters" "clusters" {}
+
+resource "nutanix_virtual_machine" "test" {
+  name                 = "cd-ubuntuserver"
+  cluster_uuid         = data.nutanix_clusters.clusters.entities.0.metadata.uuid
+  description          = "terraforming yo ahv"
+  num_vcpus_per_socket = 2
+  num_sockets          = 1
+  memory_size_mib      = 4096
+}
+
+variable "username" {
+  description = "admin username for Nutanix Cluster"
+}
+
+variable "password" {
+  description = "admin password for Nutanix Cluster"
+}
+
+variable "endpoint" {
+  description = "ip or hostname for Nutanix Cluster"
+}
---
+++ create_image/linux_image.tf
@@ -0,0 +1,30 @@
+provider "nutanix" {
+  username = var.username
+  password = var.password
+  endpoint = var.endpoint
+  insecure = true
+  port     = 9440
+}
+
+resource "nutanix_image" "ubuntumini" {
+  name        = "Ubuntu"
+  description = "UbuntuMini"
+  source_uri  = "http://archive.ubuntu.com/ubuntu/dists/bionic/main/installer-amd64/current/images/netboot/mini.iso"
+}
+
+
+data "nutanix_image" "ubuntumini" {
+    image_id = nutanix_image.ubuntumini.id
+}
+
+data "nutanix_image" "ubuntumini" {
+    image_name = nutanix_image.ubuntumini.name
+}
+
+output "image_id" {
+  value = nutanix_image.ubuntumini.id
+}
+
+output "image_name" {
+  value = nutanix_image.ubuntumini.id
+}
