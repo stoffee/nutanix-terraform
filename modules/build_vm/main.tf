5c97ad34ebb2ebd64601fcc645e6f582fca21e72
stoffee <chris@roadkill.org>
2019-10-10 14:22:01 -0700

    adding subnet

--- modules/build_vm/main.tf
+++ modules/build_vm/main.tf
@@ -37,9 +37,9 @@ resource "nutanix_virtual_machine" "test" {
   num_vcpus_per_socket = 2
   num_sockets          = 1
   memory_size_mib      = 4096
-  nic_list = [{
+  nic_list = {
       subnet_uuid = "${data.nutanix_subnet.net-1.id}"
-  }]
+  }
 }
 
 
