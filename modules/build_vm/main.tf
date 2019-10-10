bb756749d2a4cd1f1e2e286c39cf87e710a07415
stoffee <chris@roadkill.org>
2019-10-10 14:31:28 -0700

    subnet_uuid

--- modules/build_vm/main.tf
+++ modules/build_vm/main.tf
@@ -30,16 +30,22 @@ data "nutanix_subnet" "net-1" {
 #  subnet_id = "06e1e545-6b80-4a69-823d-6d080204af28"
 }
 
-resource "nutanix_virtual_machine" "test" {
+resource "nutanix_virtual_machine" "linux" {
   name                 = "cd-ubuntuserver"
   cluster_uuid         = data.nutanix_clusters.clusters.entities.0.metadata.uuid
   description          = "terraforming yo ahv"
+  subnet_uuid = "${data.nutanix_subnet.net-1.id}"
+#  nic_list = [
+#                {
+#                    subnet_reference = {
+#                        kind = "subnet"
+#                        uuid = "${data.nutanix_subnet.net-1.id}"
+#                    }
+#                }  
+#            ]
   num_vcpus_per_socket = 2
   num_sockets          = 1
   memory_size_mib      = 4096
-  nic_list = {
-      subnet_uuid = "${data.nutanix_subnet.net-1.id}"
-  }
 }
 
 
