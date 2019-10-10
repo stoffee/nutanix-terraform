1882314cb0a8d70c97aada98fc5d9a86b9fb2110
stoffee <chris@roadkill.org>
2019-10-10 13:04:17 -0700

    adding vars

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "username" {
+  description = "admin username for Nutanix Cluster"
+}
+
+variable "password" {
+  description = "admin password for Nutanix Cluster"
+}
+
+variable "endpoint" {
+  description = "ip or hostname for Nutanix Cluster"
+}
+
+variable "image_description" {
+  description = "description of OS image in Nutanix"
+  default = "Ubuntu Mini Latest"
+}
+
+variable "image_name" {
+  description = "name os OS image in Nutanix"
+  default = "UbuntuMini-Latest"
+}
+
+variable "image_source_url" {
+  description = "URL to ISO image"
+  default  = "http://archive.ubuntu.com/ubuntu/dists/bionic/main/installer-amd64/current/images/netboot/mini.iso"
+}
