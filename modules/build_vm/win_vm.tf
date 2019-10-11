1fcfb10d6872b202a9bb3ade02a2a393e77ea464
stoffee <chris@roadkill.org>
2019-10-10 19:47:23 -0700

    never give up

--- modules/build_vm/win_vm.tf
+++ modules/build_vm/win_vm.tf
@@ -1,18 +1,18 @@
-resource "random_pet" "petservername" {
+resource "random_pet" "win_petservername" {
 }
 
 data "nutanix_image" "windows_vm" {
   win_image_id = var.image_id
 }
 
-data "nutanix_clusters" "clusters" {}
-data "nutanix_subnet" "net-1" {
+data "nutanix_clusters" "winclusters" {}
+data "nutanix_subnet" "winnet-1" {
   subnet_id = var.subnet_id
 }
 
-resource "nutanix_virtual_machine" "vm" {
-  name         = random_pet.petservername.id
-  cluster_uuid = data.nutanix_clusters.clusters.entities.0.metadata.uuid
+resource "nutanix_virtual_machine" "win_vm" {
+  name         = random_pet.win_petservername.id
+  cluster_uuid = data.nutanix_clusters.winclusters.entities.0.metadata.uuid
   description  = "terraforming yo ahv"
   num_vcpus_per_socket = 2
   num_sockets          = 1
