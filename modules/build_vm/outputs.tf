705721911f249ee1a04af9b6b4429ccbf5e890ca
stoffee <chris@roadkill.org>
2019-10-10 18:56:10 -0700

    added vm set

--- modules/build_vm/outputs.tf
+++ modules/build_vm/outputs.tf
@@ -1,3 +1,3 @@
 output "ip_address" {
-  value = nutanix_virtual_machine.linux.nic_list_status.0.ip_endpoint_list[0]["ip"]
+  value = nutanix_virtual_machine.vm.nic_list_status.0.ip_endpoint_list[0]["ip"]
 }
